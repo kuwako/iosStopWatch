fc14d0eac01f23c04253c2dc3f41be13d9f481dd
kuwako <kuwako@leverages.jp>
2016-03-02 18:57:59 +0900

    ストップウォッチスタートまでできる

--- iosStopWatchApp/ViewController.swift
+++ iosStopWatchApp/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    
+    var startTime: NSTimeInterval? = nil
+    var timer: NSTimer? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +28,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func update() {
+        if let t = self.startTime {
+            let time: Double = NSDate.timeIntervalSinceReferenceDate() - t
+            let sec: Int = Int(time)
+            let msec: Int = Int((time - Double(sec)) * 100.0)
+            
+            self.timerLabel.text = NSString(format: "%02d:%02d:%02d", sec/60, sec%60, msec) as String
+        }
+        
+    }
+    
+    @IBAction func startTimer(sender: AnyObject) {
+        self.startTime =
+            NSDate.timeIntervalSinceReferenceDate()
+        self.timer =
+            NSTimer.scheduledTimerWithTimeInterval(0.01, target: self, selector: Selector("update"), userInfo: nil, repeats: true)
+        
+    }
+    
+    @IBAction func stopTimer(sender: AnyObject) {
+    }
+    
+    @IBAction func resetTimer(sender: AnyObject) {
+    }
 
 }
 
